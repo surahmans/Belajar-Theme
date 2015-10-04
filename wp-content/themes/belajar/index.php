84bc2b53551312a9d03047e2c4f4c74ddd333106
Surahman <surahman.duang@gmail.com>
2015-10-04 11:28:50 +0700

    pindahkan post pada index ke content.php

--- wp-content/themes/belajar/index.php
+++ wp-content/themes/belajar/index.php
@@ -326,73 +326,7 @@
 									<div class="seperator-post-bottom"></div>
 								</div>
 
-								<!-- STANDARD POST -->
-								<div class="ol-content-post wow fadeInUp" data-wow-duration="0.4s">
-									<!-- all meta post -->
-									<div class="all-meta-post">
-										<div class="meta-post-date">08</div>
-										<div class="meta-post-month">
-											March, 2015
-											<span class="seperator-post">/</span>
-											<a href="#">3 Comments</a>
-											<span class="seperator-post">/</span>
-											Posted By <a href="#">Zahira</a>
-										</div>
-									</div>
-									<!-- image post -->
-									<div class="wrap-image-post">
-										<a href="#">
-											<img src="http://placehold.it/850x600" alt="">
-											<div class="overlay-image-post"></div>
-										</a>
-									</div>
-									<!-- meta category -->
-									<div class="wrap-meta-category-post">
-										<span class="meta-cat-post">
-											<a href="#">Style</a>
-										</span>
-									</div>
-									<!-- title post -->
-									<div class="title-post">
-										<a href="#">In outer aspect, Pip and Dough-Boy made a match Lorem Ipsum</a>
-									</div>
-									<!-- desc post -->
-									<div class="desc-post">
-										<p>
-											Blorem Quisque id efficitur nibh, in volutpat erat. Morbi vel facilisis nulla, id posuere velit. Maecenas tempor lorem ut molestie malesuada. Nulla facilisi. Pellentesqu fermentum felis vel lacinia pulvinar, erat neque tincidunt sapien, sit amet varius lacus justo id nisi. Nunc euismod lacus lectus, ac ullamcorper neque lacinia eget justo consectetur egestas.Phasellus et odio et justo .Nunc euismod lacus lectus, ac ullamcorper neque lacinia eget justo consectetur egestas.Phasellus et odio et justo.just waiting for you to explore and discover its wonders. Gone are the days when women could not travel alone; these days all it takes is a passport and a sense of adventure for the single woman to achieve her dream of seeing the world.
-										</p>
-									</div>
-									<div class="read-share">
-										<div class="row">
-											<div class="col-xs-8">
-												<div class="wrap-readmore">
-													<a href="#"><span class="readmore-post">Continue Reading</span></a>
-												</div>
-											</div>
-											<div class="col-xs-4">
-												<div class="wrap-share-post">
-													<span class="share-post"><i class="fa fa-share-alt"></i></span>
-												</div>
-											</div>
-										</div>
-									</div>
-									<!-- share after click share -->
-									<div class="wrap-share-click-social">
-										<ul>
-											<li class="share-facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
-											<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-											<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-											<li><a href="#"><i class="fa fa-youtube"></i></a></li>
-											<li><a href="#"><i class="fa fa-rss"></i></a></li>
-											<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-											<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-											<li><a href="#"><i class="fa fa-youtube"></i></a></li>
-											<li><a href="#"><i class="fa fa-rss"></i></a></li>
-										</ul>
-									</div>
-									<!-- seperator -->
-									<div class="seperator-post-bottom"></div>
-								</div>
+								
 
 								<!-- Pagination -->
 								<div class="wrap-pagination-index">
