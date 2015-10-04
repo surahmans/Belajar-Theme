2bd0cd6f7e38861b742708d98078f160b9084dad
Surahman <surahman.duang@gmail.com>
2015-10-04 11:25:53 +0700

    pindahkan bagian sidebar di html ke sidebar.php

---
+++ wp-content/themes/belajar/sidebar.php
@@ -0,0 +1,337 @@
+<div class="col-md-3 col-md-pull-9">
+				<div class="wrap-ol-sidebar">
+					<!-- About Author -->
+					<div class="sidebarwidget">
+						<div class="sidebar-title">
+							about me
+						</div>
+						<div class="wrap-boxed-widget">
+							<div class="wrap-img-author">
+								<img src="http://placehold.it/270x270" alt="">
+								<div class="overlay-img-author"></div>
+							</div>
+							<div class="name-author-widget">
+								Zahira Salsabila
+							</div>
+							<hr class="line-author">
+							<div class="desc-author-widget">
+								In vitae ligula tristique, commodo justo sit amet, venenatis turpis. Maecenas condimentum, erat non fermentum facilisis.In vitae ligula tristique, commodo justo sit amet.
+							</div>
+						</div>
+					</div>
+
+					<!-- Pupular Post -->
+					<div class="sidebarwidget">
+						<div class="sidebar-title">
+							popular post
+						</div>
+						<div class="wrap-boxed-widget">
+							<!-- Popular 1 -->
+							<div class="wrap-popular-post">
+								<div class="popular-img">
+									<img src="http://placehold.it/270x350" alt="">
+									<div class="popular-overlay"></div>
+								</div>
+								<div class="popular-meta-date">
+									Published on 27 March, 2015
+								</div>
+								<div class="popular-line-date"></div>
+								<div class="popular-title">
+									<a href="#">In outer aspect, Pip and Dough-Boy made a match</a>
+								</div>
+							</div>
+
+							<!-- Popular 2 -->
+							<div class="wrap-popular-post">
+								<div class="popular-img">
+									<img src="http://placehold.it/270x350" alt="">
+									<div class="popular-overlay"></div>
+								</div>
+								<div class="popular-meta-date">
+									Published on 27 March, 2015
+								</div>
+								<div class="popular-line-date"></div>
+								<div class="popular-title">
+									<a href="#">Family Adventure Holidays in Moliets - Ideal for Surf Lovers</a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<!-- Social Network -->
+					<div class="sidebarwidget">
+						<div class="sidebar-title">
+							Network
+						</div>
+						<div class="wrap-boxed-widget">
+							<div class="wrap-social-network">
+								<ul>
+									<li class="social-n-fb"><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-youtube"></i></a></li>
+									<li><a href="#"><i class="fa fa-rss"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+									<li><a href="#"><i class="fa fa-vimeo-square"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<!-- Categories -->
+					<div class="sidebarwidget">
+						<div class="sidebar-title">
+							categories
+						</div>
+						<div class="wrap-boxed-widget">
+							<div class="wrap-categories-widget">
+								<ul>
+									<li><a href="#">Audio Post</a>[14]</li>
+									<li><a href="#">Aside Post</a>[8]</li>
+									<li><a href="#">Gallery Post</a>[19]</li>
+									<li><a href="#">Video Post</a>[34]</li>
+									<li><a href="#">Quote Post</a>[10]</li>
+									<li><a href="#">Link Post</a>[11]</li>
+									<li><a href="#">Standard Post</a>[15]</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<!-- Recent Article -->
+					<div class="sidebarwidget">
+						<div class="sidebar-title">
+							recent article
+						</div>
+						<div class="wrap-boxed-widget">
+							<!-- Recent 1 -->
+							<div class="wrap-recent-post">
+								<div class="row">
+									<div class="col-xs-4 cus-re-img">
+										<div class="recent-post-img">
+											<img src="http://placehold.it/100x100" alt="">
+											<div class="overlay-recent-post"></div>
+										</div>
+									</div>
+									<div class="col-xs-8 cus-re-desc">
+										<div class="recent-title">
+											<a href="#">Family Adventure Holidays in Moliets - Ideal for Surf Lovers</a>
+										</div>
+										<div class="recent-post-by">
+											Posted By <a href="#">Zahira</a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<!-- Recent 2 -->
+							<div class="wrap-recent-post">
+								<div class="row">
+									<div class="col-xs-4 cus-re-img">
+										<div class="recent-post-img">
+											<img src="http://placehold.it/100x100" alt="">
+											<div class="overlay-recent-post"></div>
+										</div>
+									</div>
+									<div class="col-xs-8 cus-re-desc">
+										<div class="recent-title">
+											<a href="#">Summer Family Adventure Holidays to British Columbia</a>
+										</div>
+										<div class="recent-post-by">
+											Posted By <a href="#">Zahira</a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<!-- Recent 3 -->
+							<div class="wrap-recent-post">
+								<div class="row">
+									<div class="col-xs-4 cus-re-img">
+										<div class="recent-post-img">
+											<img src="http://placehold.it/100x100" alt="">
+											<div class="overlay-recent-post"></div>
+										</div>
+									</div>
+									<div class="col-xs-8 cus-re-desc">
+										<div class="recent-title">
+											<a href="#">Single Trip Holiday Insurance For the Single Woman</a>
+										</div>
+										<div class="recent-post-by">
+											Posted By <a href="#">Zahira</a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<!-- Recent 4 -->
+							<div class="wrap-recent-post">
+								<div class="row">
+									<div class="col-xs-4 cus-re-img">
+										<div class="recent-post-img">
+											<img src="http://placehold.it/100x100" alt="">
+											<div class="overlay-recent-post"></div>
+										</div>
+									</div>
+									<div class="col-xs-8 cus-re-desc">
+										<div class="recent-title">
+											<a href="#">Family Adventure Holidays in Moliets - Ideal for Surf Lovers</a>
+										</div>
+										<div class="recent-post-by">
+											Posted By <a href="#">Zahira</a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+					<!-- Recent Comments -->
+					<div class="sidebarwidget">
+						<div class="sidebar-title">
+							Recent Comments
+						</div>
+						<div class="wrap-boxed-widget">
+							<div class="wrap-recent-comments">
+								<ul>
+									<li>
+										<span class="recent-comment-name">Nattasha Claudya ON</span>
+										<a href="#">Family Adventure Holidays in Moliets - Ideal for Surf Lovers</a>
+									</li>
+
+									<li>
+										<span class="recent-comment-name">Pevita Pearce ON</span>
+										<a href="#">Summer Family Adventure Holidays to British Columbia</a>
+									</li>
+
+									<li>
+										<span class="recent-comment-name">Nabilah Azalia ON</span>
+										<a href="#">Adventure Holidays in Moliets Beautiful scenery & best holidays this month</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<!-- Instagram -->
+					<div class="sidebarwidget">
+						<div class="sidebar-title">
+							instagram
+						</div>
+						<div class="wrap-boxed-widget">
+							<!-- instagram big img -->
+							<div class="instagram-big-img">
+								<a href="#">
+									<img src="http://placehold.it/270x160" alt="">
+									<div class="overlay-instagram-big">
+									</div>
+								</a>
+							</div>
+							<!-- instagram big img -->
+							<div class="instagram-big-img">
+								<a href="#">
+									<img src="http://placehold.it/270x160" alt="">
+									<div class="overlay-instagram-big">
+									</div>
+								</a>
+							</div>
+							<!-- instagram small img -->
+							<div class="instagram-small-img">
+								<a href="#"><img src="http://placehold.it/135x135" alt="">
+									<div class="overlay-instagram-small">
+									</div>
+								</a>
+							</div>
+
+							<!-- instagram small img -->
+							<div class="instagram-small-img">
+								<a href="#"><img src="http://placehold.it/135x135" alt="">
+									<div class="overlay-instagram-small">
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+
+					<!-- Archives -->
+					<div class="sidebarwidget">
+						<div class="sidebar-title">
+							archives
+						</div>
+						<div class="wrap-boxed-widget">
+							<div class="wrap-categories-widget">
+								<ul>
+									<li><a href="#">January 2015</a>[14]</li>
+									<li><a href="#">February Post</a>[8]</li>
+									<li><a href="#">March Post</a>[19]</li>
+									<li><a href="#">April Post</a>[34]</li>
+									<li><a href="#">May Post</a>[10]</li>
+									<li><a href="#">June Post</a>[11]</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<!-- Tags -->
+					<div class="sidebarwidget">
+						<div class="sidebar-title">
+							Tags
+						</div>
+						<div class="wrap-boxed-widget">
+							<div class="wrap-tags">
+								<a href="#">Travel</a>
+								<a href="#">London</a>
+								<a href="#">Fashion</a>
+								<a href="#">Clothes</a>
+								<a href="#">Shoes</a>
+								<a href="#">Food</a>
+								<a href="#">Holiday</a>
+								<a href="#">Travel</a>
+								<a href="#">London</a>
+								<a href="#">Fashion</a>
+								<a href="#">Clothes</a>
+								<a href="#">Shoes</a>
+								<a href="#">Food</a>
+								<a href="#">Holiday</a>
+							</div>
+						</div>
+					</div>
+
+					<!-- Subscribe -->
+					<div class="sidebarwidget">
+						<div class="sidebar-title">
+							Subscribe
+						</div>
+						<div class="wrap-boxed-widget">
+							<div class="subscribe-widget">
+								<p>Subscribe to our email newsletter.</p>
+								<div class="subscribe-area">
+									<form action="#">
+										<input type="text" placeholder="Enter your e-mail address">
+										<input type="submit" class="button subscribe" name="submit" value="Subscribe">
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<!-- Ads -->
+					<div class="sidebarwidget">
+						<div class="sidebar-title">
+							Ads
+						</div>
+						<div class="wrap-boxed-widget">
+							<div class="wrap-ads-widget">
+								<img src="http://placehold.it/240x400" alt="">
+								<div class="overlay-ads-widget">
+									<span class="ads-click">
+										<a href="#">Click</a>
+									</span>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
