b8a8dc0feac0c678475127832cea55551037de77
Surahman <surahman.duang@gmail.com>
2015-10-04 11:02:15 +0700

    salin struktur html untuk footer

---
+++ wp-content/themes/belajar/footer.php
@@ -0,0 +1,72 @@
+<div class="wrap-footer">
+				<div class="wrap-instagram-footer">
+					<div class="title-instagram-footer">
+						<div class="insta-title">Follow On Instagram</div>
+						<div class="line-insta-footer"></div>
+					</div>
+					<div class="container">
+						<div class="wrap-insta-4">
+							<div class="insta-4-top">
+								<div class="insta-4-top-big">
+									<a href="">
+										<img src="http://placehold.it/500x166" alt="">
+										<div class="overlay-insta-big"></div>
+									</a>
+								</div>
+								<div class="insta-4-top-small">
+									<a href="">
+										<img src="http://placehold.it/227x166" alt="">
+										<div class="overlay-insta-small"></div>
+									</a>
+								</div>
+							</div>
+							<div class="insta-4-bottom">
+								<div class="insta-4-top-small">
+									<a href="">
+										<img src="http://placehold.it/227x166" alt="">
+										<div class="overlay-insta-small"></div>
+									</a>
+								</div>
+								<div class="insta-4-top-big">
+									<a href="">
+										<img src="http://placehold.it/500x166" alt="">
+										<div class="overlay-insta-big"></div>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="wrap-insta-1">
+							<a href="">
+								<img src="http://placehold.it/500x450" alt="">
+								<div class="overlay-insta-1"></div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="wrap-footer-bg">
+					<div class="wrap-logo-footer">
+						<div class="logo-top logo-footer">
+							<a href="#">
+								<img src="images/logo.png" alt="">
+							</a>
+							<div class="desc-logo-head">
+								<a href="#">
+									Wordpress Personal Blog
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="copyright-bottom">
+						Copyright 2015 All Rights Reserved. Zahira Theme Designed by Onelabs Studio
+					</div>
+				</div>
+			</div>
+
+		<!-- ================================== end id container ============================== -->
+		</div>
+
+		<div class="el_totop eltophide"><i class="fa fa-angle-up"></i></div>
+		<!-- <a href="#" class="scrollup"><i class="fa fa-chevron-up"></i></a> -->
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	</body>
+</html>
